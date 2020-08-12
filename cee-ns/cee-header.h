a2a6e4772cb5036fdaeef5ff8139f5578bd548b8
mlite <mlite@users.noreply.github.com>
2020-08-12 12:30:51 -0700

    remove the actual code as they are in seperate repos

--- cee-ns/cee-header.h
+++
@@ -1,5 +0,0 @@
-#undef DATA_OFFSET
-#undef FIND_HEADER
-#define DATA_OFFSET (__builtin_offsetof(struct S(header), _))
-#define FIND_HEADER(p)  (struct S(header) *)((void *)((char *)(p) - DATA_OFFSET))
-#define ZERO_CEE_SECT(s)  do{ memset(s, 0, sizeof(struct cee::sect)); } while(0);
--- cee/cee-internal.h
+++
@@ -1,8 +0,0 @@
-#ifndef CEE_INTERNAL_H
-#define CEE_INTERNAL_H
-
-#ifndef NULL
-#define NULL     ((void *)0)
-#endif 
-
-#endif // CEE_INTERNAL_H
