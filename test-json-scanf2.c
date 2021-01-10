4f6477230e6d2f8db581d38bd699053353944e1a
LucasMull <muller.lucas@Hotmail.com>
2021-01-10 14:02:52 -0300

    refactor: add assert error checking, formatting changes

--- test-json-scanf2.c
+++ test-json-scanf2.c
@@ -31,95 +31,25 @@
 #include "json-scanf.h"
 
 
-FILE *select_output(int argc, char *argv[]);
-char *get_json_text(char filename[]);
+int main(void) {
+  char *locale = setlocale(LC_CTYPE, "");
+  assert(NULL != locale);
 
-int main(int argc, char *argv[]) {
-    char *locale = setlocale(LC_CTYPE, "");
-    assert(NULL != locale);
+  int integer1=0, integer2=0;
+  char str1[25] = {0};
+  char * str = "{ \"t\":\"abc\", \"s\":10, \"op\":100 }";
 
-    //FILE *f_out = select_output(argc, argv);
-    //char *json_text = get_json_text(argv[1]);
-    //char *buffer = NULL;
+  printf("%s\n", str);
 
-    //jscon_item_t *item[3] = {NULL};
-    int integer1=0, integer2=0;
-    char str1[25] = {0};
-    char * str = "{ \"t\":\"abc\", \"s\":10, \"op\":100 }";
+  json_scanf2(str,
+       "[t]%s"
+       "[s]%d"
+       "[op]%d",
+       str1,
+       &integer1,
+       &integer2);
 
-    printf("%s\n", str);
+  printf("t %s, s %d, op %d\n", str1, integer1, integer2);
 
-    json_scanf2(str,
-	       "[t]%s"
-	       "[s]%d"
-	       "[op]%d",
-	       str1,
-	       &integer1,
-	       &integer2);
-
-    printf ("t %s, s %d, op %d\n", str1, integer1, integer2);
-
-    //free(json_text);
-    //fclose(f_out);
-
-    return EXIT_SUCCESS;
-}
-
-FILE *select_output(int argc, char *argv[])
-{
-    char *p_arg=NULL;
-    while (argc--){
-        p_arg = *argv++;
-        if ((*p_arg++ == '-') && (*p_arg++ == 'o') && (*p_arg == '\0')){
-            assert(1 == argc); //check if theres exactly one arg left
-
-            char *file = *argv;
-            assert(access(file, W_OK)); //check if file exists
-
-            return fopen(file, "w");
-        }
-    }
-
-    return fopen("data.txt", "w");
-}
-
-/* returns file size in long format */
-static long
-fetch_filesize(FILE *p_file)
-{
-    fseek(p_file, 0, SEEK_END);
-    long filesize = ftell(p_file);
-    assert(filesize > 0);
-    fseek(p_file, 0, SEEK_SET);
-
-    return filesize;
-}
-
-/* returns file content */
-static char*
-read_file(FILE* p_file, long filesize)
-{
-    char *buffer = malloc(filesize+1);
-    assert(NULL != buffer);
-
-    //read file into buffer
-    fread(buffer,1,filesize,p_file);
-    buffer[filesize] = 0;
-
-    return buffer;
-}
-
-/* returns buffer containing file content */
-char*
-get_json_text(char filename[])
-{
-    FILE *file = fopen(filename, "rb");
-    assert(NULL != file);
-
-    long filesize = fetch_filesize(file);
-    char *buffer = read_file(file, filesize);
-
-    fclose(file);
-
-    return buffer;
+  return EXIT_SUCCESS;
 }
