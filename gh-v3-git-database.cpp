aeaf771c65158532b26093e85fd08bb1c3465de8
LucasMull <muller.lucas@Hotmail.com>
2021-01-21 12:31:45 -0300

    refactor: organize code

--- gh-v3-git-database.cpp
+++ gh-v3-git-database.cpp
@@ -1,43 +1,47 @@
 #include "json_scanf.h"
 
-namespace github { namespace v3 { namespace git_database {
+namespace github { 
+namespace v3 {
+namespace git_database {
 
-  namespace create_a_blob {
-    struct data {
-        char * accept;
-        char * owner;
-        char * repo;
-        char * content;
-        char * encoding;
-    };
-    struct response {
-        char * url;
-        char * sha;
-    };
+namespace create_a_blob {
 
-    static struct response *
-    load_ret_object (void * p, char * str, size_t len)
-    {
-      struct response * rep = p;
-      json_scanf(str, len, "[url]%?s [sha]%?s", &rep->url, &rep->sha);
-      return rep;
-    }
+struct data {
+    char *accept;
+    char *owner;
+    char *repo;
+    char *content;
+    char *encoding;
+};
+struct response {
+    char *url;
+    char *sha;
+};
 
-    bool run (user_agent::data * ua, struct data * d)
-    {
-      struct response * rep = calloc(1, sizeof(*rep));
-      char * post_field [2] = { 0 };
+static struct response*
+load_ret_object(void *p, char *str, size_t len)
+{
+  struct response *rep = p;
+  json_scanf(str, len, "[url]%?s [sha]%?s", &rep->url, &rep->sha);
+  return rep;
+}
 
-      post_field[0] = d->content;
-      user_agent::run(ua,
-                      rep,
-                      load_ret_object,
-                      post_field,
-                      "POST",
-                      "/repos/%s/%s/git/blobs",
-                      d->owner,
-                      d->repo);
-    }
-  }
+bool 
+run(user_agent::data *ua, struct data *d)
+{
+  struct response *rep = calloc(1, sizeof(*rep));
+  char *post_field[2] = { 0 };
 
-}}}
+  post_field[0] = d->content;
+  user_agent::run(ua,
+                  rep,
+                  load_ret_object,
+                  post_field,
+                  POST, "/repos/%s/%s/git/blobs", d->owner, d->repo);
+}
+
+} // namespace create_a_blob
+
+} // namespace git_database
+} // namespace v3
+} // namespace github
