9854a28e5d6a7966c16fe673071d5b554d903a28
mlite <mlite@users.noreply.github.com>
2021-02-10 20:34:22 -0800

    add a test example to test json_load from json files without running a bot

---
+++ test/test-json-load.cpp
@@ -0,0 +1,17 @@
+#include <libdiscord.h>
+#include "orka-utils.h"
+
+int main (int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "%s <json-file>\n", argv[0]);
+    return 1;
+  }
+
+  size_t len = 0;
+  char * json = orka_load_whole_file(argv[1], &len);
+
+  discord::guild::member::dati ** p;
+  discord::guild::member::json_list_load(json, len, &p);
+  return 0;
+}
+
