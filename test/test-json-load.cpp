bf17a2445dc2eb393ea1654e3d6c6be3d1080dab
mlite <mlite@users.noreply.github.com>
2021-02-19 18:27:41 -0800

    refactor: test EDDBAPI

--- test/test-json-load.cpp
+++ test/test-json-load.cpp
@@ -1,5 +1,9 @@
-#include <libdiscord.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <orka-debug.h>
 #include "orka-utils.h"
+#include "json-scanf.h"
+#include "json-actor.h"
 
 int main (int argc, char ** argv) {
   if (argc != 2) {
@@ -10,8 +14,14 @@ int main (int argc, char ** argv) {
   size_t len = 0;
   char * json = orka_load_whole_file(argv[1], &len);
 
+  /*
   discord::guild::member::dati ** p;
   discord::guild::member::list_from_json(json, len, &p);
+   */
+
+  int i;
+  json_extract(json, len, "(total):d", &i);
+  fprintf(stderr, "%d", i);
   return 0;
 }
 
