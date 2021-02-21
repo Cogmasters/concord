2d81dc563adb2b88756ab56c0be86921d8d6e2b8
LucasMull <muller.lucas@Hotmail.com>
2021-02-21 14:50:37 -0300

    wip: printing requested info, still needs formatting

--- bots/bot-elitebgs.cpp
+++ bots/bot-elitebgs.cpp
@@ -12,12 +12,43 @@
 /* ELITEBGS User Agent for performing connections to the API */
 orka::user_agent::dati elitebgs_ua;
 
+struct doc_s {
+  char name[512];
+  char government[512];
+  char updated_at[512];
+};
+
+struct faction_presence_s {
+  char state[512];
+  char influence[512];
+  char happiness[512];
+  char updated_at[512];
+};
+
+struct state_s {
+  char state[512];
+  char trend[512];
+};
+
+
 void embed_from_json(char *str, size_t len, void *p_embed)
 {
   using namespace discord::channel::embed;
+
   dati *embed = (dati*)p_embed;
 
-  struct sized_buffer **docs = NULL;
+  struct doc_s *doc = (struct doc_s*)malloc(sizeof *doc);
+  struct sized_buffer **l_docs = NULL; // get docs token from JSON
+
+  struct faction_presence_s *fpresence = (struct faction_presence_s*)malloc(sizeof *fpresence);
+  struct sized_buffer **l_fpresence = NULL; // get faction_presence token from JSON
+
+  struct state_s *state = (struct state_s*)malloc(sizeof *state);
+  struct sized_buffer **l_active_states = NULL; // get active_states token from JSON
+  struct sized_buffer **l_pending_states = NULL; // get pending_states token from JSON
+  struct sized_buffer **l_recovering_states = NULL; // get recovering_states token from JSON
+
+
   int total, page, pages, pagingCounter;
   bool hasPrevPage, hasNextPage;
   char *prevPage, *nextPage;
@@ -31,7 +62,7 @@ void embed_from_json(char *str, size_t len, void *p_embed)
      "[hasNextPage]%b"
      "[prevPage]%?s"
      "[nextPage]%?s",
-     &docs,
+     &l_docs,
      &total,
      &page,
      &pages,
@@ -43,125 +74,124 @@ void embed_from_json(char *str, size_t len, void *p_embed)
 
   /* @todo add some checks here */
 
-  if(!docs) return; /* early return if no docs found */
-
-  struct sized_buffer **faction_presence = NULL;
-  char  government[512],
-        name[512],
-        updated_at[512];
-
-  for (size_t i=0; docs[i]; ++i) 
+  if(l_docs) 
   {
-    json_scanf(docs[i]->start, docs[i]->size,
-        "[faction_presence]%L"
-        "[government]%s"
-        "[name]%s"
-        "[updated_at]%s",
-        &faction_presence,
-        government,
-        name,
-        updated_at);
+    char *field_value = (char*)calloc(1, 1024); //Max embed's field value length
 
-    add_field(embed, "Government", government, true);
-    add_field(embed, "Name", name, true);
-    add_field(embed, "Updated at", updated_at, true);
+    for (size_t i=0; l_docs[i]; ++i) 
+    {
+      json_scanf(l_docs[i]->start, l_docs[i]->size,
+          "[name]%S"
+          "[government]%S"
+          "[faction_presence]%L"
+          "[updated_at]%S",
+          doc->name,
+          doc->government,
+          &l_fpresence,
+          doc->updated_at);
 
-    if (faction_presence) {
-      struct sized_buffer system_name = {0},
-                          state       = {0},
-                          influence   = {0},
-                          happiness   = {0},
-                          updated_at  = {0};
-
-      struct sized_buffer **active_states     = NULL, 
-                          **pending_states    = NULL,
-                          **recovering_states = NULL,
-                          **conflicts         = NULL;
-
-      for (size_t j=0; faction_presence[j]; ++j)
+      if (l_fpresence[0]) 
       {
-        json_scanf(faction_presence[j]->start, faction_presence[j]->size,
-            "[system_name]%T"
-            "[state]%T"
-            "[influence]%T"
-            "[happiness]%T"
-            "[active_states]%L"
-            "[pending_states]%L"
-            "[recovering_states]%L"
-            "[conflicts]%L"
-            "[updated_at]%T",
-            &system_name,
-            &state,
-            &influence,
-            &happiness,
-            &active_states,
-            &pending_states,
-            &recovering_states,
-            &conflicts,
-            &updated_at);
+        for (size_t j=0; l_fpresence[j]; ++j)
+        {
+          json_scanf(l_fpresence[j]->start, l_fpresence[j]->size,
+              "[state]%S"
+              "[influence]%S"
+              "[happiness]%S"
+              "[active_states]%L"
+              "[pending_states]%L"
+              "[recovering_states]%L"
+              "[updated_at]%S",
+              fpresence->state,
+              fpresence->influence,
+              fpresence->happiness,
+              &l_active_states,
+              &l_pending_states,
+              &l_recovering_states,
+              fpresence->updated_at);
 
-        if (active_states) {
-          struct sized_buffer state = {0};
-          for (size_t k=0; active_states[k]; ++k) 
-          {
-            json_scanf(active_states[k]->start, active_states[k]->size,
-                "[state]%T", &state);
-          }
-          free(active_states);
-        }
-        if (pending_states) {
-          struct sized_buffer state = {0};
-          for (size_t k=0; pending_states[k]; ++k) 
-          {
-            json_scanf(pending_states[k]->start, pending_states[k]->size,
-                "[state]%T", &state);
-          }
-          free(pending_states);
-        }
-        if (recovering_states) {
-          struct sized_buffer state = {0},
-                              trend = {0};
-          for (size_t k=0; recovering_states[k]; ++k) 
-          {
-            json_scanf(pending_states[k]->start, pending_states[k]->size,
-                "[state]%T [trend]%T", &state, &trend);
-          }
-          free(recovering_states);
-        }
-        if (conflicts) {
-          struct sized_buffer type          = {0},
-                              status        = {0},
-                              opponent_name = {0},
-                              stake         = {0},
-                              days_won      = {0};
+          int ret = snprintf(field_value, 1024, 
+                      "State: %s\n"
+                      "Influence: %s\n"
+                      "Happiness: %s\n",
+                      fpresence->state,
+                      fpresence->influence,
+                      fpresence->happiness);
 
-          for (size_t k=0; conflicts[k]; ++k) 
+          ret += snprintf(&field_value[ret], 1024 - ret, "Active States:");
+          if (l_active_states[0]) 
           {
-            json_scanf(pending_states[k]->start, pending_states[k]->size,
-                "[type]%T"
-                "[status]%T"
-                "[opponent_name]%T"
-                "[stake]%T"
-                "[days_won]%T",
-                &type, 
-                &status,
-                &opponent_name,
-                &stake,
-                &days_won);
+            for (size_t k=0; l_active_states[k]; ++k) 
+            {
+              json_scanf(l_active_states[k]->start, l_active_states[k]->size,
+                  "[state]%S", state->state);
+              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
+            }
+            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
           }
-          free(conflicts);
+          else {
+            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
+          }
+
+          ret += snprintf(&field_value[ret], 1024 - ret, "Pending States:");
+          if (l_pending_states[0]) 
+          {
+            for (size_t k=0; l_pending_states[k]; ++k) 
+            {
+              json_scanf(l_pending_states[k]->start, l_pending_states[k]->size,
+                  "[state]%S", state->state);
+              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
+            }
+            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
+          }
+          else {
+            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
+          }
+
+          ret += snprintf(&field_value[ret], 1024 - ret, "Recovering States:");
+          if (l_recovering_states[0]) 
+          {
+            for (size_t k=0; l_recovering_states[k]; ++k) 
+            {
+              json_scanf(l_recovering_states[k]->start, l_recovering_states[k]->size,
+                  "[state]%S [trend]%S", state->state, state->trend);
+              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
+              //@todo use trend
+            }
+            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
+          }
+          else {
+            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
+          }
         }
+
+        free(l_active_states);
+        l_active_states = NULL;
+
+        free(l_pending_states);
+        l_pending_states = NULL;
+
+        free(l_recovering_states);
+        l_recovering_states = NULL;
       }
 
-      free(faction_presence);
+      free(l_fpresence);
+      l_fpresence = NULL;
+
+      add_field(embed, doc->name, field_value, false);
     }
+    free(field_value);
   }
+
+  free(doc);
+  free(fpresence);
+  free(state);
 
   if (prevPage)
     free(prevPage);
   if (nextPage)
     free(nextPage);
-  free(docs);
+  free(l_docs);
 }
 
 void on_ready(discord::client *client, const discord::user::dati *me)
