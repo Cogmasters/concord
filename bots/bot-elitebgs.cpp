f3072d665d73c169d00a55dec901842e854d4de1
LucasMull <muller.lucas@Hotmail.com>
2021-02-21 18:23:33 -0300

    wip: working on printing influence difference between ticks

--- bots/bot-elitebgs.cpp
+++ bots/bot-elitebgs.cpp
@@ -19,8 +19,9 @@ struct doc_s {
 };
 
 struct faction_presence_s {
+  char system_id[512];
   char state[512];
-  char influence[512];
+  float influence;
   char happiness[512];
   char updated_at[512];
 };
@@ -41,7 +42,9 @@ void embed_from_json(char *str, size_t len, void *p_embed)
   struct sized_buffer **l_docs = NULL; // get docs token from JSON
 
   struct faction_presence_s *fpresence = (struct faction_presence_s*)malloc(sizeof *fpresence);
+  struct faction_presence_s *history = (struct faction_presence_s*)malloc(sizeof *history);
   struct sized_buffer **l_fpresence = NULL; // get faction_presence token from JSON
+  struct sized_buffer **l_history = NULL; // get recovering_states token from JSON
 
   struct state_s *state = (struct state_s*)malloc(sizeof *state);
   struct sized_buffer **l_active_states = NULL; // get active_states token from JSON
@@ -74,116 +77,141 @@ void embed_from_json(char *str, size_t len, void *p_embed)
 
   /* @todo add some checks here */
 
-  if(l_docs) 
+  char field_value[EMBED_FIELD_VALUE_LEN];
+
+  for (size_t i=0; l_docs[i]; ++i) 
   {
-    char *field_value = (char*)calloc(1, 1024); //Max embed's field value length
+    json_scanf(l_docs[i]->start, l_docs[i]->size,
+        "[name]%S"
+        "[government]%S"
+        "[faction_presence]%L"
+        "[updated_at]%S"
+        "[history]%L",
+        doc->name,
+        doc->government,
+        &l_fpresence,
+        doc->updated_at,
+        &l_history);
 
-    for (size_t i=0; l_docs[i]; ++i) 
+    if (l_fpresence[0]) 
     {
-      json_scanf(l_docs[i]->start, l_docs[i]->size,
-          "[name]%S"
-          "[government]%S"
-          "[faction_presence]%L"
-          "[updated_at]%S",
-          doc->name,
-          doc->government,
-          &l_fpresence,
-          doc->updated_at);
-
-      if (l_fpresence[0]) 
+      for (size_t j=0; l_fpresence[j]; ++j)
       {
-        for (size_t j=0; l_fpresence[j]; ++j)
+        json_scanf(l_fpresence[j]->start, l_fpresence[j]->size,
+            "[system_id]%S"
+            "[state]%S"
+            "[influence]%f"
+            "[happiness]%S"
+            "[active_states]%L"
+            "[pending_states]%L"
+            "[recovering_states]%L"
+            "[updated_at]%S",
+            fpresence->system_id,
+            fpresence->state,
+            &fpresence->influence,
+            fpresence->happiness,
+            &l_active_states,
+            &l_pending_states,
+            &l_recovering_states,
+            fpresence->updated_at);
+
+        for (size_t j2=0; l_history[j2]; ++j2)
         {
-          json_scanf(l_fpresence[j]->start, l_fpresence[j]->size,
-              "[state]%S"
-              "[influence]%S"
-              "[happiness]%S"
-              "[active_states]%L"
-              "[pending_states]%L"
-              "[recovering_states]%L"
+          json_scanf(l_history[j2]->start, l_history[j2]->size,
+              "[system_id]%S"
+              "[influence]%f"
               "[updated_at]%S",
-              fpresence->state,
-              fpresence->influence,
-              fpresence->happiness,
-              &l_active_states,
-              &l_pending_states,
-              &l_recovering_states,
-              fpresence->updated_at);
+              history->system_id,
+              &history->influence,
+              history->updated_at);
 
-          int ret = snprintf(field_value, 1024, 
-                      "State: %s\n"
-                      "Influence: %s\n"
-                      "Happiness: %s\n",
-                      fpresence->state,
-                      fpresence->influence,
-                      fpresence->happiness);
-
-          ret += snprintf(&field_value[ret], 1024 - ret, "Active States:");
-          if (l_active_states[0]) 
-          {
-            for (size_t k=0; l_active_states[k]; ++k) 
-            {
-              json_scanf(l_active_states[k]->start, l_active_states[k]->size,
-                  "[state]%S", state->state);
-              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
-            }
-            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
-          }
-          else {
-            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
-          }
-
-          ret += snprintf(&field_value[ret], 1024 - ret, "Pending States:");
-          if (l_pending_states[0]) 
-          {
-            for (size_t k=0; l_pending_states[k]; ++k) 
-            {
-              json_scanf(l_pending_states[k]->start, l_pending_states[k]->size,
-                  "[state]%S", state->state);
-              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
-            }
-            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
-          }
-          else {
-            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
-          }
-
-          ret += snprintf(&field_value[ret], 1024 - ret, "Recovering States:");
-          if (l_recovering_states[0]) 
-          {
-            for (size_t k=0; l_recovering_states[k]; ++k) 
-            {
-              json_scanf(l_recovering_states[k]->start, l_recovering_states[k]->size,
-                  "[state]%S [trend]%S", state->state, state->trend);
-              ret += snprintf(&field_value[ret], 1024 - ret, " %s,", state->state);
-              //@todo use trend
-            }
-            ret += snprintf(&field_value[ret], 1024 - ret, "\n");
-          }
-          else {
-            ret += snprintf(&field_value[ret], 1024 - ret, " None\n");
-          }
+          if (0 == strcmp(history->system_id, fpresence->system_id))
+            break;
         }
 
-        free(l_active_states);
-        l_active_states = NULL;
+        float influence_diff = 100*(fpresence->influence - history->influence);
+        char *influence_emoji;
+        if (influence_diff > 0)
+          influence_emoji = "📈";
+        else if (influence_diff < 0)
+          influence_emoji = "📉";
+        else
+          influence_emoji = "🔷";
 
-        free(l_pending_states);
-        l_pending_states = NULL;
+        int ret = snprintf(field_value, sizeof(field_value), 
+                    "State: %s\n"
+                    "Influence: %.1f%s%.1f\n"
+                    "Happiness: %s\n",
+                    fpresence->state,
+                    fpresence->influence * 100, influence_emoji, influence_diff,
+                    fpresence->happiness);
 
-        free(l_recovering_states);
-        l_recovering_states = NULL;
+        ret += snprintf(&field_value[ret], sizeof(field_value) - ret, "Active States:");
+        if (l_active_states[0]) 
+        {
+          for (size_t k=0; l_active_states[k]; ++k) 
+          {
+            json_scanf(l_active_states[k]->start, l_active_states[k]->size,
+                "[state]%S", state->state);
+            ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " %s,", state->state);
+          }
+          field_value[ret-1] = '\n'; //replace end comma with newline
+        }
+        else {
+          ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " none\n");
+        }
+
+        ret += snprintf(&field_value[ret], sizeof(field_value) - ret, "Pending States:");
+        if (l_pending_states[0]) 
+        {
+          for (size_t k=0; l_pending_states[k]; ++k) 
+          {
+            json_scanf(l_pending_states[k]->start, l_pending_states[k]->size,
+                "[state]%S", state->state);
+            ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " %s,", state->state);
+          }
+          field_value[ret-1] = '\n'; //replace end comma with newline
+        }
+        else {
+          ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " none\n");
+        }
+
+        ret += snprintf(&field_value[ret], sizeof(field_value) - ret, "Recovering States:");
+        if (l_recovering_states[0]) 
+        {
+          for (size_t k=0; l_recovering_states[k]; ++k) 
+          {
+            json_scanf(l_recovering_states[k]->start, l_recovering_states[k]->size,
+                "[state]%S [trend]%S", state->state, state->trend);
+            ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " %s,", state->state);
+            //@todo use trend
+          }
+          field_value[ret-1] = '\n'; //replace end comma with newline
+        }
+        else {
+          ret += snprintf(&field_value[ret], sizeof(field_value) - ret, " none\n");
+        }
       }
 
-      free(l_fpresence);
-      l_fpresence = NULL;
+      free(l_active_states);
+      l_active_states = NULL;
 
-      add_field(embed, doc->name, field_value, false);
+      free(l_pending_states);
+      l_pending_states = NULL;
+
+      free(l_recovering_states);
+      l_recovering_states = NULL;
     }
-    free(field_value);
+
+    free(l_history);
+    free(l_fpresence);
+    l_fpresence = NULL;
+
+    add_field(embed, doc->name, field_value, true);
   }
 
   free(doc);
+  free(history);
   free(fpresence);
   free(state);
 
@@ -222,7 +250,10 @@ void on_command(
 
   char query[512];
   int ret = query_inject(query, sizeof(query),
-              "(system):s", msg->content);
+              "(system):s"
+              "(count):1", 
+              msg->content);
+
   ASSERT_S(ret < (int)sizeof(query), "Out of bounds write attempt");
 
   /* Fetch from ELITEBGS API */
