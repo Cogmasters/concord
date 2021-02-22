575564bdd3d198114c518bdeb68da1fb8fda4777
mlite <mlite@users.noreply.github.com>
2021-02-22 09:45:01 -0800

    test: add another test to test other combinations of k/v

---
+++ test/test-json-injector-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "json-actor.h"
+#include <string.h>
+#include <stdlib.h>
+
+int foobar (char * pos, size_t size, void *p)
+{
+  if (NULL == p) {
+    return snprintf(pos, size, "{}");
+  }
+  else {
+    int * i = (int *)p;
+    return snprintf(pos, size, "%d, %d", *i, *i+1);
+  }
+}
+
+static char bigbuf[1024];
+int main ()
+{
+  void * A[2] = {0};
+  char * content = "This is the content.";
+
+  json_inject(bigbuf, sizeof(bigbuf), "(content):s @", content, A, sizeof(A));
+  fprintf(stderr, "%s\n", bigbuf);
+
+  A[0] = content;
+  json_inject(bigbuf, sizeof(bigbuf), "(content):s @A", content, A, sizeof(A));
+  fprintf(stderr, "%s\n", bigbuf);
+
+  //content = NULL;
+  A[0] = NULL;
+  json_inject(bigbuf, sizeof(bigbuf), "(content):s @A", content, A, sizeof(A));
+  fprintf(stderr, "%s\n", bigbuf);
+
+  return 0;
+}
