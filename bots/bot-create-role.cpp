2fe709498fb5462a333331c09a86795fc3dac427
LucasMull <muller.lucas@Hotmail.com>
2021-02-25 17:29:11 -0300

    wip: bot-create-role.cpp

---
+++ bots/bot-create-role.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <libdiscord.h>
+#include "orka-utils.h" // for orka_timestamp_ms()
+
+using namespace discord;
+
+void on_ready(client *client, const user::dati *me)
+{
+  fprintf(stderr, "\n\nCreate-Role-Bot succesfully connected to Discord as %s#%s!\n\n",
+      me->username, me->discriminator);
+
+  (void)client;
+}
+
+void on_command(
+    client *client,
+    const user::dati *me,
+    const channel::message::dati *msg)
+{
+  // make sure bot doesn't echoes other bots
+  if (msg->author->bot)
+    return;
+
+  role::dati *role = role::dati_alloc();
+
+  guild::create_role::params params1 = {
+    .name = msg->content
+  };
+  //guild::create_role::run(client, msg->guild_id, &params1, role); @todo should work with role parameter
+  guild::create_role::run(client, msg->guild_id, &params1, NULL);
+
+  if (role->id) {
+    char text[150];
+    snprintf(text, sizeof(text), "Succesfully created %s role", msg->content);
+
+    channel::message::create::params params2 = {
+      .content = text
+    };
+    channel::message::create::run(client, msg->channel_id, &params2, NULL);
+  }
+
+  role::dati_free(role);
+}
+
+int main(int argc, char *argv[])
+{
+  const char *config_file;
+  if (argc > 1)
+    config_file = argv[1];
+  else
+    config_file = "bot.config";
+
+  global_init();
+
+  client *client = fast_init(config_file);
+  assert(NULL != client);
+
+  setcb(client, COMMAND, &on_command, "!create ");
+
+  printf("\n\nThis bot demonstrates how easy it is to create a"
+         " new role.\n"
+         "1. Type !create <name of role>"
+         "\nTYPE ANY KEY TO START BOT\n");
+  fgetc(stdin); // wait for input
+
+
+  run(client);
+
+  cleanup(client);
+
+  global_cleanup();
+}
+
+
