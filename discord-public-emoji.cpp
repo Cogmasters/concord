fb7356667ec628b602843a9b2bc26ad5349e38de
LucasMull <muller.lucas@Hotmail.com>
2021-03-06 12:01:25 -0300

    feat: add emoji::get_list() to fetch emojis from a server

---
+++ discord-public-emoji.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <libdiscord.h>
+#include <orka-utils.h>
+
+namespace discord {
+namespace emoji {
+
+dati**
+get_list(client *client, const uint64_t guild_id)
+{
+  if (!guild_id) {
+    D_PUTS("Missing 'guild_id'");
+    return NULL;
+  }
+
+  dati **new_emojis = NULL;
+
+  struct resp_handle resp_handle =
+    {&dati_list_from_json_v, (void*)&new_emojis};
+
+  user_agent::run( 
+    &client->ua,
+    &resp_handle,
+    NULL,
+    HTTP_GET,
+    "/guilds/%llu/emojis", guild_id);
+
+  return new_emojis;
+}
+
+} // namespace emoji
+} // namespace discord
+
