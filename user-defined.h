64e4ee2f108cce5e0902817170642b9c41a33130
mlite <mlite@users.noreply.github.com>
2021-03-23 09:22:00 -0700

    feat: rename a function

--- user-defined.h
+++ user-defined.h
@@ -2,12 +2,10 @@
 #define USER_DEFINED_H
 // user-defined functions
 
-namespace discord {
-namespace user_defined {
-namespace bulk_delete_messages {
-void run(struct discord_client *client, u64_snowflake_t channel_id, u64_snowflake_t author_id);
-}
-}
-}
+void
+discord_user_defined_bulk_delete_message(
+  struct discord_client *client,
+  u64_snowflake_t channel_id,
+  u64_snowflake_t author_id);
 
 #endif //USER_DEFINED_H
