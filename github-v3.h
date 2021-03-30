3a2a50b048d7f7dd0e5a6b177ec3f1a54c74707e
mlite <mlite@users.noreply.github.com>
2021-03-30 12:56:56 -0700

    fix: for newer gcc

--- github-v3.h
+++ github-v3.h
@@ -6,17 +6,17 @@ struct github_v3_git_op_file {
   char *sha;
 };
 
-struct github_v3_git_op* 
+extern struct github_v3_git_op*
 github_v3_git_op_init(char *username, char *token, char *repo_config);
 
-char * github_v3_git_op_update_my_fork(struct github_v3_git_op *d);
-char* github_v3_git_op_get_head_commit(struct github_v3_git_op *data);
-char* github_v3_git_op_get_tree_sha(struct github_v3_git_op *data, char *commit_sha);
-void github_v3_git_op_create_blobs(struct github_v3_git_op *data, struct github_v3_git_op_file **files);
-char* github_v3_git_op_create_tree(struct github_v3_git_op *data, char *base_tree_sha, struct github_v3_git_op_file **files);
-char* github_v3_git_op_create_a_commit(struct github_v3_git_op *data, char *tree_sha, char *parent_commit_sha, char *commit_msg);
-void github_v3_git_op_create_a_branch(struct github_v3_git_op *data, char *head_commit_sha, char *branch);
-void github_v3_git_op_update_a_commit(struct github_v3_git_op *d, char *branch, char *commit_sha);
-void github_v3_git_op_create_a_pull_request(struct github_v3_git_op *d, char *branch, char *pull_msg);
+extern char * github_v3_git_op_update_my_fork(struct github_v3_git_op *d);
+extern char* github_v3_git_op_get_head_commit(struct github_v3_git_op *data);
+extern char* github_v3_git_op_get_tree_sha(struct github_v3_git_op *data, char *commit_sha);
+extern void github_v3_git_op_create_blobs(struct github_v3_git_op *data, struct github_v3_git_op_file **files);
+extern char* github_v3_git_op_create_tree(struct github_v3_git_op *data, char *base_tree_sha, struct github_v3_git_op_file **files);
+extern char* github_v3_git_op_create_a_commit(struct github_v3_git_op *data, char *tree_sha, char *parent_commit_sha, char *commit_msg);
+extern void github_v3_git_op_create_a_branch(struct github_v3_git_op *data, char *head_commit_sha, char *branch);
+extern void github_v3_git_op_update_a_commit(struct github_v3_git_op *d, char *branch, char *commit_sha);
+extern void github_v3_git_op_create_a_pull_request(struct github_v3_git_op *d, char *branch, char *pull_msg);
 
 #endif // GITHUB_V3_H
