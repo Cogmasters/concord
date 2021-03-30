d48cdc0adc8196a07908d7521519e5fdd1ccb6ec
mlite <mlite@users.noreply.github.com>
2021-03-30 13:11:44 -0700

    fix: warning for asprintf

--- common/json-scanf.c
+++ common/json-scanf.c
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
