4abaeea50894e80ff9cc479fda826bfa22a4bf49
LucasMull <muller.lucas@Hotmail.com>
2021-04-18 17:51:28 -0300

    fix: regen-specs-code.sh

--- scripts/regen-specs-code.sh
+++ scripts/regen-specs-code.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mypath=$(dirname $(readlink -f $0))
 pushd $mypath/..
-make purge
-make specs_clean
-make clean_actor_gen
-make actor-gen.exe
-make all_headers
-make specs
+make purge && 
+make specs_clean &&
+make clean_actor_gen &&
+make actor-gen.exe &&
+make all_headers &&
+make specs &&
 popd
