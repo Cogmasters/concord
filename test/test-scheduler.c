0390a34f8ca12a396f8ffbee6869a1e4602f82a4
LucasMull <muller.lucas@Hotmail.com>
2021-04-21 19:18:41 -0300

    refactor: fix comment

--- test/test-scheduler.c
+++ test/test-scheduler.c
@@ -16,7 +16,7 @@ int main(void)
 {
   struct task_s *task1 = task_init();
   struct task_s *task2 = task_init();
-  task_start(task1, 2000, 1000, NULL, &print_hello); //start 5 seconds from now, and repeat every 2 seconds
+  task_start(task1, 2000, 1000, NULL, &print_hello); //start 2 seconds from now, and repeat every 1 second
   task_start(task2, 0, 500, NULL, &print_bye); //start immediatelly, and repeat every half second
 
   sleep(10);
