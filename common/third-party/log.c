c9689b72ceeeb0a4feb19782119fd90156fb20e5
mlite <mlite@users.noreply.github.com>
2021-04-28 03:41:50 -0700

    feat: need tid in log files too

--- common/third-party/log.c
+++ common/third-party/log.c
@@ -73,8 +73,8 @@ static void file_callback(log_Event *ev) {
   char buf[64];
   buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
   fprintf(
-    ev->udata, "%s %-5s %s:%d: ",
-    buf, level_strings[ev->level], ev->file, ev->line);
+    ev->udata, "%s.%010u %-5s %s:%d: ",
+    buf, (unsigned)pthread_self(), level_strings[ev->level], ev->file, ev->line);
   vfprintf(ev->udata, ev->fmt, ev->ap);
   fprintf(ev->udata, "\n");
   fflush(ev->udata);
