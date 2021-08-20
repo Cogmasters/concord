18e0424e2f27d4735a9d0ea372b90a53051cd31c
LucasMull <muller.lucas@Hotmail.com>
2021-08-20 12:37:18 -0300

    fix(specs-code): github

--- specs-code/github/all_enums.h
+++
@@ -1,6 +0,0 @@
-/* This file is generated from specs/github/github-user.json, Please don't edit it. */
-/**
- * @file specs-code/github/all_enums.h
- * @see https://docs.github.com/en/rest/reference/users#get-a-user
- */
-
