9ebf87b0fe97c34fa36362d39f2a69a6a85a60d0
lcsmuller <muller.lucas@Hotmail.com>
2022-01-04 17:09:31 -0300

    fix: implicit stdlib include

--- examples/bot-github-gist.c
+++ examples/bot-github-gist.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "github.h"
 
 void print_usage()
--- test/test-websockets.c
+++ test/test-websockets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
