8279e09e4ed4ce60febe2fd7ee39babb4ca62ce1
Anotra <Anotra9999@gmail.com>
2022-05-28 23:29:28 -0400

    fix(discord-gateway_dispatch.c): determine correct interval for next heartbeat

--- src/discord-gateway_dispatch.c
+++ src/discord-gateway_dispatch.c
@@ -240,7 +240,9 @@ on_ping_timer_cb(struct discord *client, struct discord_timer *timer)
     struct discord_gateway *gw = timer->data;
     if (~timer->flags & DISCORD_TIMER_CANCELED) {
         discord_gateway_perform(gw);
-        timer->interval = (int64_t)gw->timer->interval;
+        const u64unix_ms next_hb = gw->timer->hbeat + gw->timer->interval;
+        timer->interval = (int64_t)(next_hb - discord_timestamp(client));
+        if (timer->interval < 0) timer->interval = 0;
         timer->repeat = 1;
     }
 }
