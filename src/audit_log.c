0d8c1f47bd97facd98b387b1eeb3be996dad03a4
ThePedroo <pedroolimpio152@gmail.com>
2023-02-05 18:24:12 -0300

    feat(queries): Improve queries system
    Use QuerieC library to improve query system.
    
    Co-authored-by: Lucas Müller <lucas@muller.codes>

--- src/audit_log.c
+++ src/audit_log.c
@@ -5,6 +5,7 @@
 #include "discord.h"
 #include "discord-internal.h"
 #include "discord-request.h"
+#include "queriec.h"
 
 CCORDcode
 discord_get_guild_audit_log(struct discord *client,
@@ -17,37 +18,37 @@ discord_get_guild_audit_log(struct discord *client,
 
     CCORD_EXPECT(client, guild_id != 0, CCORD_BAD_PARAMETER, "");
 
-    if (params) {
-        int offset = 0;
+    struct queriec queriec;
+    queriec_init(&queriec, sizeof(query));
 
+    if (params) {
+        int res;
+
+        char buf[32];
         if (params->user_id) {
-            offset += snprintf(query + offset, sizeof(query) - (size_t)offset,
-                               "?user_id=%" PRIu64, params->user_id);
-            ASSERT_NOT_OOB(offset, sizeof(query));
+            res = queriec_snprintf_add(&queriec, query, "user_id", sizeof("user_id"),
+                                       buf, sizeof(buf), "%" PRIu64, params->user_id);
+            ASSERT_S(res != QUERIEC_ERROR_NOMEM, "Out of bounds write attempt");
         }
         if (params->action_type) {
-            offset += snprintf(query + offset, sizeof(query) - (size_t)offset,
-                               "%saction_type=%d", *query ? "&" : "?",
-                               params->action_type);
-            ASSERT_NOT_OOB(offset, sizeof(query));
+            res = queriec_snprintf_add(&queriec, query, "action_type", sizeof("action_type"),
+                                       buf, sizeof(buf), "%d", params->action_type);
+            ASSERT_S(res != QUERIEC_ERROR_NOMEM, "Out of bounds write attempt");
         }
         if (params->before) {
-            offset += snprintf(query + offset, sizeof(query) - (size_t)offset,
-                               "%sbefore=%" PRIu64, *query ? "&" : "?",
-                               params->before);
-            ASSERT_NOT_OOB(offset, sizeof(query));
+            res = queriec_snprintf_add(&queriec, query, "before", sizeof("before"),
+                                       buf, sizeof(buf), "%" PRIu64, params->before);
+            ASSERT_S(res != QUERIEC_ERROR_NOMEM, "Out of bounds write attempt");
         }
         if (params->limit) {
-            offset +=
-                snprintf(query + offset, sizeof(query) - (size_t)offset,
-                         "%slimit=%d", *query ? "&" : "?", params->limit);
-            ASSERT_NOT_OOB(offset, sizeof(query));
+            res = queriec_snprintf_add(&queriec, query, "limit", sizeof("limit"),
+                                       buf, sizeof(buf), "%d", params->limit);
+            ASSERT_S(res != QUERIEC_ERROR_NOMEM, "Out of bounds write attempt");
         }
     }
 
     DISCORD_ATTR_INIT(attr, discord_audit_log, ret, NULL);
 
     return discord_rest_run(&client->rest, &attr, NULL, HTTP_GET,
-                            "/guilds/%" PRIu64 "/audit-logs%s", guild_id,
-                            query);
+                            "/guilds/%" PRIu64 "/audit-logs%s", guild_id, query);
 }
