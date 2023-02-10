cda7fae8a0613e0e996e4fe71cdfb4272cf20484
Naveen Albert <asterisk@phreaknet.org>
2023-02-10 20:30:57 +0000

    fix: allow async client shutdowns for multithreading safety.

--- include/concord-once.h
+++ include/concord-once.h
@@ -6,20 +6,18 @@
 
 #ifndef CONCORD_ONCE_H
 
-/**
- * @brief If `SIGINT` is detected client(s) will be disconnected from their
- *      on-going session
- *
- * This global shall be set if a `SIGINT` is detected, running clients will
- *      then attempt to perform a clean disconnect, rather then just letting
- *      the program end abruptly.
- * @note client shall only attempt to disconnect if there aren't any active
- *      events waiting to be listened or reacted to
- */
-extern int ccord_has_sigint;
-
 /** @brief Asynchronously shutdown all client(s) from their on-going sessions */
 void ccord_shutdown_async();
+
+/**
+ * @brief Whether or not concord is currently shutting down
+ *
+ * If true, clients will then attempt to perform a clean disconnect, rather than
+ *    just letting the program end abruptly (e.g. in the case of a SIGINT).
+ * @note client shall only attempt to disconnect if there aren't any active
+ *    events waiting to be listened or reacted to
+ */
+int ccord_shutting_down();
 
 /**
  * @brief Initialize global shared-resources not API-specific
