b97a5281bbe1eb9ba785ca3018976c4894448bec
ThePedroo <pedroolimpioguerra@gmail.com>
2026-06-14 16:42:28 -0300

    chore(curl): replace `curl_multi_socket_all` for `curl_multi_socket_action`
    This commit replaces the deprecated function "curl_multi_socket_all" for "curl_multi_socket_action". With this comes some changes to use event-driven way of "curl_multi_socket_action". One of them includes storing "now" before any callback since it we cannot reliably save them inside "ws_multi_socket_run". We also update "io_poller.c" to replace it too.

--- core/io_poller.c
+++ core/io_poller.c
@@ -121,6 +121,10 @@ io_poller_poll(struct io_poller *io, int milliseconds)
     return poll(io->pollfds, io->cnt, milliseconds);
 }
 
+static void io_curl_cb(struct io_poller *io,
+                         enum io_poller_events events,
+                         void *user_data);
+
 int
 io_poller_perform(struct io_poller *io)
 {
@@ -132,7 +136,16 @@ io_poller_perform(struct io_poller *io)
             if (io->pollfds[i].revents & POLLOUT) events |= IO_POLLER_OUT;
             io->pollfds[i].revents = 0;
             struct io_poller_element *element = &io->elements[i];
-            element->cb(io, events, element->user_data);
+            if (element->cb == io_curl_cb) {
+                int cevents = 0;
+                if (events & IO_POLLER_IN) cevents |= CURL_CSELECT_IN;
+                if (events & IO_POLLER_OUT) cevents |= CURL_CSELECT_OUT;
+                struct io_curlm *curlm = element->user_data;
+                curl_multi_socket_action(curlm->multi, io->pollfds[i].fd,
+                                         cevents, &curlm->running);
+                curlm->should_perform = true;
+            } else
+                element->cb(io, events, element->user_data);
         }
     }
     for (int i = 0; i < io->curlm_cnt; i++) {
@@ -140,11 +153,13 @@ io_poller_perform(struct io_poller *io)
         if (curlm->should_perform
             || (-1 != curlm->timeout && now >= curlm->timeout)) {
             curlm->should_perform = false;
+            if (-1 != curlm->timeout && now >= curlm->timeout)
+                curl_multi_socket_action(curlm->multi, CURL_SOCKET_TIMEOUT,
+                                         0, &curlm->running);
             int result =
                 curlm->cb
                     ? curlm->cb(io, curlm->multi, curlm->user_data)
-                    : curl_multi_socket_all(curlm->multi, &curlm->running);
-
+                    : 0;
             if (result != 0) return result;
         }
     }
--- src/discord-rest_request.c
+++ src/discord-rest_request.c
@@ -454,11 +454,6 @@ _discord_response_data_cleanup(struct discord *client, void *p_response_data)
 CCORDcode
 discord_requestor_info_read(struct discord_requestor *rqtor)
 {
-    int alive = 0;
-
-    if (CURLM_OK != curl_multi_socket_all(rqtor->mhandle, &alive))
-        return CCORD_CURLM_INTERNAL;
-
     /* ask for any messages/informationals from the individual transfers */
     while (1) {
         int msgq = 0;
